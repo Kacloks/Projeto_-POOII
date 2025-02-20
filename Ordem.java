abadc268c759e14009b3196d3f7dd74b70bbf8fd
Kacloks <117231721+Kacloks@users.noreply.github.com>
2025-02-20 08:23:59 -0300

    Create Ordem.java

---
+++ Ordem.java
@@ -0,0 +1,13 @@
+package pacotegame;
+
+import java.awt.Color;
+
+public class Ordem {
+    int posicao;
+    Color cor;
+
+    public Ordem(int posicao, Color cor) {
+        this.posicao = posicao;
+        this.cor = cor;
+    }
+}
